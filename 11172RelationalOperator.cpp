e83f507fe86ca32a01f424213690b8ca8ee42162
Shah Tajbid Tousif <tajbidtousif@gmail.com>
2022-07-20 21:45:59 +0600

    UVa 11172

---
+++ 11172RelationalOperator.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tc;
+    cin >> tc;
+
+    while (tc--)
+    {
+        int a,b;
+
+        cin >> a >> b;
+
+        if(a>b)
+        {
+            cout << ">" << "\n";
+        }
+        else if(a<b)
+        {
+            cout << "<" << "\n";
+        }
+        else{
+            cout << "=" << "\n";
+        }
+    }
+    
+}
