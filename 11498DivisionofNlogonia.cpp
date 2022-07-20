bfdcd8ff0b38423ad383c4cb094ea6a3001c87e7
Shah Tajbid Tousif <tajbidtousif@gmail.com>
2022-07-21 01:52:41 +0600

    UVa-11498

--- 11498DivisionofNlogonia.cpp
+++ 11498DivisionofNlogonia.cpp
@@ -1,14 +1,46 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
     int tc;
-    cin >> tc;
-
-    while(tc--)
+    
+    while (true)
     {
-        int 
+        cin >> tc;
+        if(tc==0)
+        {
+            break;
+        }
+        int m, n;
+        cin >> m >> n;
+
+        while (tc--)
+        {
+            int x, y;
+            cin >> x >> y;
+
+            if (m == x || n == y)
+            {
+                cout << "divisa" << endl;
+            }
+            else if (m < x && n < y) // positive
+            {
+                cout << "NE" << endl;
+            }
+            else if (m < x && n > y)
+            {
+                cout << "SE" << endl;
+            }
+            else if (m > x && n < y)
+            {
+                cout << "NO" << endl;
+            }
+            else if (m > x && n > y)
+            {
+                cout << "SO" << endl;
+            }
+        }
     }
 
     return 0;
